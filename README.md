9fec1c133a4cba1fcf55889b1d15c1c23e79ea7a
Orion Palaquibay <orionpal@buffalo.edu>
2024-06-24 13:51:19 -0400

    changing nature of the bash script

---
+++ README.md
@@ -0,0 +1,12 @@
+Hello!
+
+The general purpose of this is: I'm not always going crazy on code, but I like my github history to look complete. SO, I'm automating some simple commits to "fluff" my github. I hope that this project can evolve into something more substantial, but for now it's just analyzing randoms words and writing definitions.
+
+
+Originally I was going to use an LLM to generate file contents, but that only worked when running individual jobs and not when running it automated.
+Now I'm using a cron-job to execute my commands
+
+Credit:
+
+wordlist: https://websites.umich.edu/~jlawler/wordlist
+dictionary: project gutenberg (gutenberg.org)
--- root.sh
+++ root.sh
@@ -1,21 +1,46 @@
-#!/bin/bash
 
-# Change directory to the location of your Python file
-cd C:/Users/13472/Desktop/AutoGitFluff/AutoFluff
+# Navigate to GitHub repository
+cd /Users/orionpal/Desktop/Projects/AutoFluff || { echo "Failed to navigate to repo" >> ~/daily_github_update.log; exit 1; }
 
-# Define variables
-PYTHON_FILE="function.py"
-GITHUB_REPO="https://github.com/orionpal/AutoFluff.git"
+# Files containing words and definitions
+WORDS_FILE=words.txt
+DICTIONARY_FILE=dictionary.txt
 
-# run file and get commit message
-COMMIT_MESSAGE=$(python "$PYTHON_FILE")
+# Count the number of lines in the file
+line_count=$(wc -l < $WORDS_FILE)
 
+# Generate a random number between 1 and the number of lines
+random_line=$((RANDOM % line_count + 1))
+# Save random word
+random_word=$(sed "${random_line}q;d" $WORDS_FILE)
+# Print the word at the random line
+echo $random_word
 
-# Add changes to git
+# Convert the random word to uppercase
+uppercase_word=$(echo $random_word | tr '[:lower:]' '[:upper:]')
+
+# Find the line number of the uppercase word in the dictionary file
+line_number=$(grep -n "^$uppercase_word$" $DICTIONARY_FILE | cut -d: -f1)
+# If the word is found, search for "Defn:" and write the definition to a file
+if [ -n "$line_number" ]; then
+    # Get the lines from the dictionary starting from the word line and write the definition to a file
+    tail -n +$line_number $DICTIONARY_FILE | awk '
+    /Defn:/ {
+        flag=1
+        sub(/.*Defn: */, "")
+        print > "'$random_word.txt'"
+        next
+    }
+    flag {
+        if (/^$/) exit
+        print >> "'$random_word.txt'"
+    }'
+else
+    # Remove the line from words.txt if the word is not found in the dictionary
+    awk 'NR != '$random_line'' $WORDS_FILE > temp && mv temp $WORDS_FILE
+    echo "Word not found in dictionary. Line removed from words.txt."
+fi
+
 git add .
-
-# Commit changes
-git commit -m "$COMMIT_MESSAGE"
-
-# Push changes to GitHub
-git push origin master
+git commit -m "Did some stuff using $random_word"
+git push origin main
